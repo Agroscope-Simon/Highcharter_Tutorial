9d124dd37538044ae72819911a752e34475551b4
Agroscope-Simon <simon.kollaart@agroscope.admin.ch>
2022-11-28 22:57:37 +0100

    added script data viz

---
+++ code/Intro_Data_Viz_1.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(highcharter)
+library(htmlwidgets)
+library(RColorBrewer)
+library(here)
+
+
+# Load data
+
+nations <- read_csv(here("data", "nations.csv")) %>%
+  mutate(gdp_tn = gdp_percap*population/1000000000000)
+  
+# prepare data
+big4 <- nations %>%
+  filter(iso3c == "CHN" | iso3c == "DEU" | iso3c == "JPN" | iso3c == "USA") %>%
+  arrange(year) 
+
+# basic symbol-and-line chart, default settings
+
+big4 %>% 
+hchart("line", hcaes(x = year, y = gdp_tn, group = country))
+                
